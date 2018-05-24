ab41029e36460bc30a59c8920b7698b5fe07db68
Ruslan <ruslan@Ruslans-iMac.local>
2018-05-24 14:33:58 +0200

    Clippo in clip.py

--- Server.py
+++ Server.py
@@ -1,70 +1,11 @@
 #!/usr/bin/env python
 
-import pyperclip, socket, threading
+import socket
 
-from random import random
-from time import sleep
-
+from clip import Clippo, STOP
 
 ADDR = ( "localhost", 17098 )
 NCLIENT = 4
-STOP = False
-
-
-class Clippo :
-    
-    def __init__( self, sock, addr ) :
-        print "Clippo(%s,%s)" % ( sock, addr )
-        self.sock = sock
-        self.addr = addr
-        # ---- HI ------------------------------------------------------
-        self.sock.send( 'hi from server\r' )
-        print self.read_command()
-        # ---- SENDER --------------------------------------------------
-        self.sender_task = threading.Thread( target=self.sender )
-        self.sender_task.start()
-        # ---- RECEIVER --------------------------------------------------
-        self.receiver()
-
-    def read_command( self ) :
-        a = ' '
-        b = ''
-        while a != '\r':
-            a = self.sock.recv(1)
-            b = b + a
-        return b
-
-    def receiver( self ) :
-        print "receiver started"
-        CLIP = pyperclip.paste()
-        while True :
-            # attendi una clipboard, se cambiata copiala
-            rcv = self.read_command().split( ' ' )
-            print "rcv=%s" % rcv
-            cmd = rcv[0]
-            if cmd == 'clipboard' :
-                size = int(rcv[1])      # A: rcv[1] rappresenta un numero intero
-                clip = self.sock.recv(size)
-        #       print "remote clipboard=%s" % clip
-                if clip != CLIP :
-                    CLIP = clip
-                    pyperclip.copy(clip)
-
-    def sender( self ) :
-        print "sender started"
-        CLIP = pyperclip.paste()
-        while True :
-            if STOP :
-                return
-            sleep( 1 + random() ) # sleep 1 + random() second
-            clip = pyperclip.paste()
-            if clip != CLIP :
-                CLIP = clip
-                self.sock.send( 'clipboard %d \r' % len(clip) )
-                self.sock.send( clip )
-
-
-
 
 
 if __name__ == '__main__' :
--- clippo.py
+++ clippo.py
@@ -1,16 +1,62 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
-import pyperclip
+import pyperclip, socket, threading
+
+from random import random
 from time import sleep
 
-# pyperclip.copy('Hello world!')
 
-CLIP = pyperclip.paste()
+STOP = False
 
-while True :
-    sleep( 1 ) # sleep 1 seconf
-    clip = pyperclip.paste()
-    if clip != CLIP :
-        CLIP = clip
-        print clip
+
+class Clippo :
     
+    def __init__( self, sock, addr ) :
+        print "Clippo(%s,%s)" % ( sock, addr )
+        self.sock = sock
+        self.addr = addr
+        # ---- HI ------------------------------------------------------
+        self.sock.send( 'hi from server\r' )
+        print self.read_command()
+        # ---- SENDER --------------------------------------------------
+        self.sender_task = threading.Thread( target=self.sender )
+        self.sender_task.start()
+        # ---- RECEIVER --------------------------------------------------
+        self.receiver()
+
+    def read_command( self ) :
+        a = ' '
+        b = ''
+        while a != '\r':
+            a = self.sock.recv(1)
+            b = b + a
+        return b
+
+    def receiver( self ) :
+        print "receiver started"
+        CLIP = pyperclip.paste()
+        while True :
+            # attendi una clipboard, se cambiata copiala
+            rcv = self.read_command().split( ' ' )
+            print "rcv=%s" % rcv
+            cmd = rcv[0]
+            if cmd == 'clipboard' :
+                size = int(rcv[1])      # A: rcv[1] rappresenta un numero intero
+                clip = self.sock.recv(size)
+        #       print "remote clipboard=%s" % clip
+                if clip != CLIP :
+                    CLIP = clip
+                    pyperclip.copy(clip)
+
+    def sender( self ) :
+        print "sender started"
+        CLIP = pyperclip.paste()
+        while True :
+            if STOP :
+                return
+            sleep( 1 + random() ) # sleep 1 + random() second
+            clip = pyperclip.paste()
+            if clip != CLIP :
+                CLIP = clip
+                self.sock.send( 'clipboard %d \r' % len(clip) )
+                self.sock.send( clip )
